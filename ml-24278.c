e4bd3ceb5d4449ddc821f0c207969de1119a9fb8
sedentaryowl <sedentaryowl@riseup.net>
2024-09-09 10:47:50 +0800

    remove ml

--- ml-24278.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-double x = 2.3;
-
-int main(int ac, char *av[]) {
-}
--- ml-27441.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-double x = 2.5;
-
-int main(int ac, char *av[]) {
-printf("%lf", x);
-}
